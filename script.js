1cb4fc2d31208d8ed76e60d315b25f6b0232d423
fadliEvan <fadliyurisman@gmail.com>
2026-03-16 02:50:39 +0700

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
-// Smooth Scroll
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-
-anchor.addEventListener('click', function(e){
-
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+anchor.addEventListener('click',function(e){
 e.preventDefault();
-
 document.querySelector(this.getAttribute('href')).scrollIntoView({
-
 behavior:'smooth'
-
 });
-
 });
-
 });
-
-
-
-// Navbar Shadow
 
 window.addEventListener("scroll",function(){
-
 let navbar=document.querySelector(".navbar");
-
 if(window.scrollY>50){
-
 navbar.style.boxShadow="0 10px 30px rgba(0,0,0,.2)";
-
 }else{
-
 navbar.style.boxShadow="none";
-
 }
-
 });
